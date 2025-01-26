172f5d46fe48fbe7439196ef0593cd6091a52bc2
HO-NGUYEN-QUYNH-HUONG <quynhhuong397999@gmail.com>
2025-01-26 16:16:41 +0700

    login

--- WebEvent/web/urls.py
+++ WebEvent/web/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_index),
+    path('', views.get_index, name='index'),
+    path('login/', views.get_login, name='login'),
 ]
